4ea77bc81306127de29b841f4410b7636ea05f0e
Vladislav <vadimromashko@gmail.com>
2022-07-11 21:57:12 +0300

    some changes

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -3,70 +3,79 @@ import api from '../api';
 
 const Users = () => {
   const [users, setUsers] = useState(api.users.fetchAll());
-  const handleDelete = (userId) => {
-    setUsers((prevState) => prevState.filter((item) => item._id !== userId));
-  };
+  const handleDelete = (userId) =>
+    setUsers(users.filter((user) => user._id !== userId));
+
   const renderPhrase = (number) => {
-    if (number >= 2 && number <= 4) {
-      return 'человека тусанут';
-    } else {
-      return 'человек тусанет';
+    const lastOne = Number(number.toString().slice(-1));
+    if (number > 4 && number < 15) {
+      return 'Человек тусанет';
+    }
+
+    if ([2, 3, 4].indexOf(lastOne) >= 0) {
+      return 'Человека тусанут';
+    }
+
+    if (lastOne === 1) {
+      return 'Человек тусанет';
     }
   };
 
-  return !users.length ? (
-    <h2>
-      <span className="badge bg-danger">{'Никто с тобой не тусанет'}</span>
-    </h2>
-  ) : (
+  return (
     <>
       <h2>
-        <span className="badge bg-primary">{`${users.length} ${renderPhrase(
-          users.length
-        )}  с тобой сегодня`}</span>
+        <span
+          className={'badge bg-' + (users.length > 0 ? 'primary' : 'danger')}
+        >
+          {users.length > 0
+            ? `${users.length} ${renderPhrase(users.length)} с тобой сегодня`
+            : 'Никто с тобой не тусанет'}
+        </span>
       </h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Имя</th>
-            <th scope="col">Качества</th>
-            <th scope="col">Профессия</th>
-            <th scope="col">Встретился, раз</th>
-            <th scope="col">Оценка</th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => {
-            return (
-              <tr key={user._id}>
-                <td>{user.name}</td>
-                <td>
-                  {user.qualities.map((quality) => (
-                    <span
-                      key={quality._id}
-                      className={'me-1 badge bg-' + quality.color}
+      {users.length > 0 && (
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качества</th>
+              <th scope="col">Профессия</th>
+              <th scope="col">Встретился, раз</th>
+              <th scope="col">Оценка</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => {
+              return (
+                <tr key={user._id}>
+                  <td>{user.name}</td>
+                  <td>
+                    {user.qualities.map((quality) => (
+                      <span
+                        key={quality._id}
+                        className={'m-1 badge bg-' + quality.color}
+                      >
+                        {quality.name}
+                      </span>
+                    ))}
+                  </td>
+                  <td>{user.profession.name}</td>
+                  <td>{user.completedMeetings}</td>
+                  <td>{`${user.rate} / 5`}</td>
+                  <td className="text-end">
+                    <button
+                      className="btn btn-danger "
+                      onClick={() => handleDelete(user._id)}
                     >
-                      {quality.name}
-                    </span>
-                  ))}
-                </td>
-                <td>{user.profession.name}</td>
-                <td>{user.completedMeetings}</td>
-                <td>{user.rate + ` / 5`}</td>
-                <td>
-                  <button
-                    className="btn btn-danger"
-                    onClick={() => handleDelete(user._id)}
-                  >
-                    delete
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+                      delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </>
   );
 };
